cbd10cc1a789eb13c4156368fbfe08aef6230569
sameera-nz7 <naazsameera.i7@gmail.com>
2026-03-28 22:58:05 +0530

    Coded UC2: Display Hello with Command-Line Argument

--- Main.java
+++ Main.java
@@ -1,7 +1,16 @@
 // Main.java
 public class Main {
     public static void main(String[] args) {
-        // Print "Hello World" to the console
-        System.out.println("Hello World");
+        String name;
+
+        // Check if a command-line argument is provided
+        if (args.length > 0) {
+            name = args[0]; // First argument
+        } else {
+            name = "World"; // Default value
+        }
+
+        // Print personalized greeting
+        System.out.println("Hello, " + name + "!");
     }
 }
